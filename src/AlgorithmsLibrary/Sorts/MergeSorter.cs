37875fc8b927358c44f8b1f5af9485b74bcb0c3d
Tuncay Altinpulluk <tuncayalt@gmail.com>
2019-07-02 15:45:18 -0400

    MergeSort tests and implementation added.

---
+++ src/AlgorithmsLibrary/Sorts/MergeSorter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsLibrary.Sorts
+{
+    public class MergeSorter<T> : ISorter<T> where T : IComparable
+    {
+        public void Sort(IList<T> list)
+        {
+            if (list == null || list.Count <= 1)
+            {
+                return;
+            }
+
+            Sort(list, 0, list.Count - 1);
+        }
+
+        private void Sort(IList<T> list, int left, int right)
+        {
+            if (left >= right)
+            {
+                return;
+            }
+
+            var mid = (left + right) / 2;
+            Sort(list, left, mid);
+            Sort(list, mid + 1, right);
+            Merge(list, left, mid, right);
+        }
+
+        private void Merge(IList<T> list, int left, int mid, int right)
+        {
+            var lo = 0;
+            var hi = mid + 1;
+            var result = new List<T>();
+            
+            while (lo <= mid && hi <= right)
+            {
+                if (list[lo].CompareTo(list[hi]) <= 0)
+                {
+                    result.Add(list[lo]);
+                    lo++;
+                }
+                else
+                {
+                    result.Add(list[hi]);
+                    hi++;
+                }
+            }
+
+            while (lo <= mid)
+            {
+                result.Add(list[lo]);
+                lo++;
+            }
+
+            while (hi <= right)
+            {
+                result.Add(list[hi]);
+                hi++;
+            }
+
+            var ind = 0;
+            for (var i = left; i <= right; i++)
+            {
+                list[i] = result[ind];
+                ind++;
+            }
+        }
+    }
+}
---
+++ src/AlgorithmsTests/SortsTests/MergeSortTests.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using AlgorithmsLibrary.Sorts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsTests.SortsTests
+{
+    [TestClass]
+    public class MergeSortTests
+    {
+        private MergeSorter<int> mergeSorter;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mergeSorter = new MergeSorter<int>();
+        }
+
+        [TestMethod]
+        public void MergeSort_NullList_NullList()
+        {
+            // Arrange
+            IList<int> list = null;
+
+            // Act
+            mergeSorter.Sort(list);
+
+            // Assert
+            Assert.AreEqual(null, list);
+        }
+
+        [TestMethod]
+        public void MergeSort_EmptyList_EmptyList()
+        {
+            // Arrange
+            var list = new List<int>();
+
+            // Act
+            mergeSorter.Sort(list);
+
+            // Assert
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [TestMethod]
+        public void MergeSort_ListWith1Element_ListSame()
+        {
+            // Arrange
+            var list = new List<int> { 1 };
+
+            // Act
+            mergeSorter.Sort(list);
+
+            // Assert
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual(1, list[0]);
+        }
+
+        [TestMethod]
+        public void MergeSort_ListWith2Elements_ListSorted()
+        {
+            // Arrange
+            var list = new List<int> { 2, 1 };
+
+            // Act
+            mergeSorter.Sort(list);
+
+            // Assert
+            Assert.AreEqual(2, list.Count);
+            AssertIfSorted(list);
+        }
+
+        [TestMethod]
+        public void MergeSort_ListWith3Elements_ListSorted()
+        {
+            // Arrange
+            var list = new List<int> { 0, 1, 0 };
+
+            // Act
+            mergeSorter.Sort(list);
+
+            // Assert
+            Assert.AreEqual(3, list.Count);
+            AssertIfSorted(list);
+        }
+
+        [TestMethod]
+        public void MergeSort_ListWithMoreElements_ListSorted()
+        {
+            // Arrange
+            var list = new List<int> { 0, 1, 0, -2, 10, 22, 22, 11, 12, 5, -2, 1, 0 };
+
+            // Act
+            mergeSorter.Sort(list);
+
+            // Assert
+            Assert.AreEqual(13, list.Count);
+            AssertIfSorted(list);
+        }
+
+        private static void AssertIfSorted(IList<int> list)
+        {
+            for (var i = 0; i < list.Count - 1; i++)
+            {
+                Assert.IsTrue(list[i] <= list[i + 1]);
+            }
+        }
+    }
+}
